cd1219de89ae8c7d693d8d8eabcdbaea19ab8459
kamesh6592-cell <kamesh6592@gmail.com>
2025-10-06 13:13:06 +0530

    Create env.js

---
+++ utils/env.js
@@ -0,0 +1,8 @@
+// utils/env.js
+export function getEnv(key) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
+}
